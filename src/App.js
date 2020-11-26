174628b3d75ed8affab144dab7e4cff379161ccc
Ricardo Cardoso <rickclsilva@gmail.com>
2020-11-25 21:58:37 -0300

    section 3 class 49

--- src/App.js
+++ src/App.js
@@ -1,11 +1,78 @@
-import './App.css';
+import React, { Component } from 'react'; 
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-    </div>
-  );
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  state = {
+    people: [
+      {
+        name: 'Carol',
+        age: 47,
+        hobbies: 'play volleyball'
+      },
+      {
+        name: "Matt",
+        age: 19
+      },
+      {
+        name: "Yuri",
+        age: 91
+      }
+    ]
+  }
+
+  switchNameHandler = (newName) => {
+    this.setState({
+      ...this.state,
+      people: [
+        {
+          name: newName,
+          age: 49,
+          hobbies: 'play volleyball on the beach'
+        }
+      ]
+    });
+  }
+
+  nameChangeHandler = (event) => {
+    this.setState({
+      ...this.state,
+      people: [
+        {
+          name: event.target.value,
+          age: 49,
+          hobbies: 'play volleyball on the beach'
+        }
+      ]
+    });
+  }
+
+  render () {
+    const peopleComponents = this.state.people.map((p, index) => {
+      let hobbies = '';
+      if (!!p.hobbies) hobbies = `My hobbies are: ${p.hobbies}!`;
+      return (
+        <Person
+          click={this.switchNameHandler.bind(this, 'Anna')}
+          change={this.nameChangeHandler}
+          name={p.name}
+          age={p.age}
+          key={index}>
+          {hobbies}
+        </Person>
+      );
+    });
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button onClick={this.switchNameHandler.bind(this, 'Anna')}>Switch name</button>
+        {peopleComponents}
+      </div>
+    );
+  }
 }
 
 export default App;
