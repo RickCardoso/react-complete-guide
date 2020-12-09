72cc47ac319a9a7564770139ae9151d4a5660db7
Ricardo Cardoso <ricardo.cardoso@loft.com.br>
2020-12-09 18:29:43 -0300

    3. assert text length and validate

--- src/ValidationComponent/ValidationComponent.js
+++ src/ValidationComponent/ValidationComponent.js
@@ -1,5 +1,7 @@
 const ValidationComponent = ({ text }) => {
-  return 'validation component'
+  if (text.length > 10) return 'text too long';
+  if (text.length < 5) return 'text too short';
+  return 'text is fine'
 }
 
 export default ValidationComponent;
